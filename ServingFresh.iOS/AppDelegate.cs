0be462dd883cc298dbce2e00f633b7a353cc722e
Carlos <omarfacio2010@gmail.com>
2020-11-10 09:27:51 -0800

    Notifications are working for iOS and Android (Manually)

--- ServingFresh.iOS/AppDelegate.cs
+++ ServingFresh.iOS/AppDelegate.cs
@@ -8,6 +8,9 @@ using UIKit;
 using Xamarin.Forms.Platform.iOS;
 using UserNotifications;
 using ServingFresh.Notifications;
+using WindowsAzure.Messaging;
+using System.Diagnostics;
+using Xamarin.Essentials;
 
 namespace ServingFresh.iOS
 {
@@ -17,8 +20,9 @@ namespace ServingFresh.iOS
     [Register("AppDelegate")]
     public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
     {
-        
 
+
+        private SBNotificationHub Hub { get; set; }
 
 
         //
@@ -75,22 +79,7 @@ namespace ServingFresh.iOS
                 UIControlState.Normal);
             UIView.AppearanceWhenContainedIn(typeof(UIAlertView)).TintColor = Color.FromHex("#a0050f").ToUIColor();
             UIView.AppearanceWhenContainedIn(typeof(UIAlertController)).TintColor = Color.FromHex("#a0050f").ToUIColor();
-            return true;
-        }
 
-        public override bool OpenUrl(UIApplication app, NSUrl url, NSDictionary options)
-        {
-            // Convert NSUrl to Uri
-            var uri = new Uri(url.AbsoluteString);
-
-            // Load redirectUrl page
-            AuthenticationState.Authenticator.OnPageLoading(uri);
-
-            return true;
-        }
-
-        public bool RegistedDeviceToPushNotifications()
-        {
             if (UIDevice.CurrentDevice.CheckSystemVersion(10, 0))
             {
                 UNUserNotificationCenter.Current.RequestAuthorization(UNAuthorizationOptions.Alert | UNAuthorizationOptions.Badge | UNAuthorizationOptions.Sound,
@@ -110,7 +99,155 @@ namespace ServingFresh.iOS
                 UIRemoteNotificationType notificationTypes = UIRemoteNotificationType.Alert | UIRemoteNotificationType.Badge | UIRemoteNotificationType.Sound;
                 UIApplication.SharedApplication.RegisterForRemoteNotificationTypes(notificationTypes);
             }
-            return UIApplication.SharedApplication.IsRegisteredForRemoteNotifications;
+            return true;
+        }
+
+        public override bool OpenUrl(UIApplication app, NSUrl url, NSDictionary options)
+        {
+            // Convert NSUrl to Uri
+            var uri = new Uri(url.AbsoluteString);
+
+            // Load redirectUrl page
+            AuthenticationState.Authenticator.OnPageLoading(uri);
+
+            return true;
+        }
+
+        //public bool RegistedDeviceToPushNotifications()
+        //{
+        //    if (UIDevice.CurrentDevice.CheckSystemVersion(10, 0))
+        //    {
+        //        UNUserNotificationCenter.Current.RequestAuthorization(UNAuthorizationOptions.Alert | UNAuthorizationOptions.Badge | UNAuthorizationOptions.Sound,
+        //                                                                (granted, error) => InvokeOnMainThread(UIApplication.SharedApplication.RegisterForRemoteNotifications));
+        //    }
+        //    else if (UIDevice.CurrentDevice.CheckSystemVersion(8, 0))
+        //    {
+        //        var pushSettings = UIUserNotificationSettings.GetSettingsForTypes(
+        //                UIUserNotificationType.Alert | UIUserNotificationType.Badge | UIUserNotificationType.Sound,
+        //                new NSSet());
+
+        //        UIApplication.SharedApplication.RegisterUserNotificationSettings(pushSettings);
+        //        UIApplication.SharedApplication.RegisterForRemoteNotifications();
+        //    }
+        //    else
+        //    {
+        //        UIRemoteNotificationType notificationTypes = UIRemoteNotificationType.Alert | UIRemoteNotificationType.Badge | UIRemoteNotificationType.Sound;
+        //        UIApplication.SharedApplication.RegisterForRemoteNotificationTypes(notificationTypes);
+        //    }
+        //    return UIApplication.SharedApplication.IsRegisteredForRemoteNotifications;
+        //}
+
+        //public override bool FinishedLaunching(UIApplication application, NSDictionary launchOptions)
+        //{
+        //    if (UIDevice.CurrentDevice.CheckSystemVersion(10, 0))
+        //    {
+        //        UNUserNotificationCenter.Current.RequestAuthorization(UNAuthorizationOptions.Alert | UNAuthorizationOptions.Badge | UNAuthorizationOptions.Sound,
+        //                                                                (granted, error) => InvokeOnMainThread(UIApplication.SharedApplication.RegisterForRemoteNotifications));
+        //    }
+        //    else if (UIDevice.CurrentDevice.CheckSystemVersion(8, 0))
+        //    {
+        //        var pushSettings = UIUserNotificationSettings.GetSettingsForTypes(
+        //                UIUserNotificationType.Alert | UIUserNotificationType.Badge | UIUserNotificationType.Sound,
+        //                new NSSet());
+
+        //        UIApplication.SharedApplication.RegisterUserNotificationSettings(pushSettings);
+        //        UIApplication.SharedApplication.RegisterForRemoteNotifications();
+        //    }
+        //    else
+        //    {
+        //        UIRemoteNotificationType notificationTypes = UIRemoteNotificationType.Alert | UIRemoteNotificationType.Badge | UIRemoteNotificationType.Sound;
+        //        UIApplication.SharedApplication.RegisterForRemoteNotificationTypes(notificationTypes);
+        //    }
+
+        //    return true;
+        //}
+
+        public override void RegisteredForRemoteNotifications(UIApplication application, NSData deviceToken)
+        {
+            Debug.WriteLine("Registered");
+            if (Preferences.Get("guid", null) != null)
+            {
+                return;
+            }
+
+            Hub = new SBNotificationHub(AppConstants.ListenConnectionString, AppConstants.NotificationHubName);
+
+            // update registration with Azure Notification Hub
+            Hub.UnregisterAll(deviceToken, (error) =>
+            {
+                if (error != null)
+                {
+                    Debug.WriteLine($"Unable to call unregister {error}");
+                    return;
+                }
+
+                var guid = Guid.NewGuid();
+                var tag = "guid_" + guid.ToString();
+                Console.WriteLine("guid:" + tag);
+                Preferences.Set("guid", tag);
+
+                var tags = new NSSet(AppConstants.SubscriptionTags.Append(tag).ToArray());
+
+                Hub.RegisterNative(deviceToken, tags, (errorCallback) =>
+                {
+                    if (errorCallback != null)
+                    {
+                        Debug.WriteLine($"RegisterNativeAsync error: {errorCallback}");
+                    }
+                });
+
+                var templateExpiration = DateTime.Now.AddDays(120).ToString(System.Globalization.CultureInfo.CreateSpecificCulture("en-US"));
+                Hub.RegisterTemplate(deviceToken, "defaultTemplate", AppConstants.APNTemplateBody, templateExpiration, tags, (errorCallback) =>
+                {
+                    if (errorCallback != null)
+                    {
+                        if (errorCallback != null)
+                        {
+                            Debug.WriteLine($"RegisterTemplateAsync error: {errorCallback}");
+                        }
+                    }
+                });
+            });
+        }
+
+        public override void ReceivedRemoteNotification(UIApplication application, NSDictionary userInfo)
+        {
+            ProcessNotification(userInfo, false);
+        }
+
+        void ProcessNotification(NSDictionary options, bool fromFinishedLaunching)
+        {
+            // Check to see if the dictionary has the aps key.  This is the notification payload you would have sent
+            if (null != options && options.ContainsKey(new NSString("aps")))
+            {
+                //Get the aps dictionary
+                NSDictionary aps = options.ObjectForKey(new NSString("aps")) as NSDictionary;
+
+                string alert = string.Empty;
+
+                //Extract the alert text
+                // NOTE: If you're using the simple alert by just specifying
+                // "  aps:{alert:"alert msg here"}  ", this will work fine.
+                // But if you're using a complex alert with Localization keys, etc.,
+                // your "alert" object from the aps dictionary will be another NSDictionary.
+                // Basically the JSON gets dumped right into a NSDictionary,
+                // so keep that in mind.
+                if (aps.ContainsKey(new NSString("alert")))
+                    alert = (aps[new NSString("alert")] as NSString).ToString();
+
+                //If this came from the ReceivedRemoteNotification while the app was running,
+                // we of course need to manually process things like the sound, badge, and alert.
+                if (!fromFinishedLaunching)
+                {
+                    //Manually show an alert
+                    if (!string.IsNullOrEmpty(alert))
+                    {
+                        var myAlert = UIAlertController.Create("Notification", alert, UIAlertControllerStyle.Alert);
+                        myAlert.AddAction(UIAlertAction.Create("OK", UIAlertActionStyle.Default, null));
+                        UIApplication.SharedApplication.KeyWindow.RootViewController.PresentViewController(myAlert, true, null);
+                    }
+                }
+            }
         }
     }
 }
---
+++ ServingFresh.iOS/iOSAzureConstants.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace ServingFresh.iOS
+{
+    public class iOSAzureConstants
+    {
+        public const string ListenConnectionString = "Endpoint=sb://serving-fresh-notification-namespace.servicebus.windows.net/;SharedAccessKeyName=DefaultListenSharedAccessSignature;SharedAccessKey=56+I0UhYCRohkoUYHgb4dHHyWTx8puGBv+PvaDLp6K8=";
+        public const string NotificationHubName = "Serving-Fresh-Notification-Hub";
+    }
+}
